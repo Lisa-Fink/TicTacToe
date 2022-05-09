b8cd5296c6a19f3053c8390fb96732a863f9a500
Lisa Fink <lisadfink@gmail.com>
2022-05-09 18:02:45 -0400

    Changed aiChoseTile from a random choice to a calculated choice

--- script.js
+++ script.js
@@ -22,7 +22,9 @@ const gameBoard = (() => {
 const gamePlay = (() => {
     let _playerTwo = 'human';
     let _end = false;
-    
+    let winners = [[0,1,2], [3,4,5], [6,7,8], 
+                [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
     const startGame = (e) => {
         e.preventDefault();
         if (e.target.form[1].value == '' | 
@@ -43,8 +45,6 @@ const gamePlay = (() => {
     };
 
     const _checkWin = () => {
-        let winners = [[0,1,2], [3,4,5], [6,7,8], 
-                [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
         for (wins of winners) {
             if ((gameBoard.arr[wins[0]] === gameBoard.arr[wins[1]] && 
                 gameBoard.arr[wins[1]] === gameBoard.arr[wins[2]]) &&
@@ -80,28 +80,88 @@ const gamePlay = (() => {
         _changeTurn();
 
         if (turn == 'O' && _playerTwo == 'AI' && _end == false) {
-            _ai_playTile();
+            _aiPlayTile();
             _endTurn();
         };
     };
 
-    const _ai_playTile = () => {
-        let aiTileIndex = _ai_choice()
-        let aiTile = document.getElementById(aiTileIndex + 1)
-        displayController.changeTile(aiTileIndex, aiTile)
+    const _aiPlayTile = () => {
+        let aiTileIndex = _aiChooseTile();
+        let idNum = Number(aiTileIndex)+ 1
+        
+        let aiTile = document.getElementById(idNum.toString());
+        
+        displayController.changeTile(aiTileIndex, aiTile);
     }
 
-    const _ai_choice = () => {
-        const availableTiles = []
-        for (let i = 0; i < gameBoard.arr.length; i++) {
-            if (gameBoard.arr[i] == 'empty') {
-                availableTiles.push(i)
+    //const _aiChooseEasy = () => {
+        // const availableTiles = []
+        // for (let i = 0; i < gameBoard.arr.length; i++) {
+        //     if (gameBoard.arr[i] == 'empty') {
+        //         availableTiles.push(i)
+        //     }
+        // }
+        // let index = availableTiles[Math.floor(Math.random() * (availableTiles.length - 1))]
+        // return index
+    //}
+
+    const _aiChooseTile = () => {
+        
+        const ranks = {
+            0: 0,
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+            6: 0,
+            7: 0,
+            8: 0
+        }
+        for (wins of winners) {
+            if ((  gameBoard.arr[wins[0]] ==   gameBoard.arr[wins[1]] |   gameBoard.arr[wins[1]] ==   gameBoard.arr[wins[2]] |   gameBoard.arr[wins[0]] ==   gameBoard.arr[wins[2]]) &&
+                (wins.filter(tile =>  gameBoard.arr[tile] == 'empty').length == 1)) {
+                if (  gameBoard.arr[wins[0]] ==   gameBoard.arr[wins[1]]) {
+                    ranks[wins[2]] += 1000
+                } else if ( gameBoard.arr[wins[1]] ==   gameBoard.arr[wins[2]]) {
+                    ranks[wins[0]] += 1000
+                } else {
+                   ranks[wins[1]] += 1000
+                }
             }
         }
-        let index = availableTiles[Math.floor(Math.random() * (availableTiles.length - 1))]
-        return index
         
+        for (wins of winners) {
+            if (wins.filter(tile =>  gameBoard.arr[tile] == 'empty').length == 2) {
+                for (let i = 0; i < 3; i++) {
+                     gameBoard.arr[wins[i]] == 'empty' ? ranks[wins[i]] += 20 : null
+                }
+            }
+        }
+        
+        for (wins of winners) {
+                if (wins.filter(tile =>  gameBoard.arr[tile] == 'empty').length == 3) {
+                    ranks[wins[0]] += 10
+                    ranks[wins[1]] += 10
+                    ranks[wins[2]] += 10
+            }
+        }
+        
+        for (wins of winners) {
+            if (wins.filter(tile =>  gameBoard.arr[tile] == 'X').length == 1) {
+                for (let i = 0; i < 3; i++) {
+                     gameBoard.arr[wins[i]] == 'empty' ? ranks[wins[i]] += 10 : null
+                }
+            }
+        }
+        
+        let choice = Object.keys(ranks).reduce((a, b) => ranks[a] > ranks[b] ? a : b);
+        
+        return choice
     }
+
+
+
 
     const togglePlayer = (e) => {
         e.preventDefault();
